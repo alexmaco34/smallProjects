59af3af68661209581103a46c09074996db2a953
alex_maco34 <alex_maco34@libero.it>
2022-03-02 14:39:43 +0100

    second two hour project

--- README.md
+++
@@ -1,4 +0,0 @@
-# smallProjects
-This repository is a little demonstration.
-Small (maybe sometimes bigger) projects will be inserted here.
-Please check out the other branches.
--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -21,26 +21,70 @@ const useStyles = makeStyles((theme) => ({
   singleSquare: {
     height: "30%",
     width: "30%",
-    backgroundColor: "cyan",
+    backgroundColor: "gray",
   },
 }))
 
 function App() {
   const classes = useStyles();
 
-  let ammountOfSquares = 9;
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[getRandomInt(16)];
+    }
+    return color;
+  }
+
+  function getTwoDifferentNumbers(max){
+    let firstNumber = getRandomInt(max);
+    let secondNumber = 0;
+    do {secondNumber = getRandomInt(max)
+    } while(secondNumber == firstNumber);
+    return [firstNumber, secondNumber]
+  }
+
+  let ammountOfSquares = 16;
+  let ammountOfColors = ammountOfSquares/2;
+  let ammountOfMoves = ammountOfSquares/2;
+  let revealed = false;
+  let squareSize = "21%";
+  let unrevealedSquareColor = "gray";
+
+  function colorSquares(numberOfSquares){
+    let squaresToReturn = {}
+    for(let i=0;i<numberOfSquares;i++){
+      let randomColor = getRandomColor()
+      twoNumbers
+      squaresToReturn.push({})
+    }
+    return squaresToReturn
+  }
+
+  let coloredSquares = colorSquares();
+  
   function createCards(){
     return(
       <Grid container className={classes.squareContainer}>
         {Array.from(Array(ammountOfSquares)).map((_, index) => (
-            <Grid key={index} className={classes.singleSquare}>
-              <div id={index}></div>
+            <Grid key={index} id={index} className={classes.singleSquare} style={{width: squareSize, height: squareSize, background: unrevealedSquareColor, }}>
             </Grid>
           ))}
       </Grid>
     )
   }
+
+  function revealSquare(){
+
+  }
+
+  document.addEventListener('mousedown', function (event) {
+  })
 
     return (
       <div className="App">
