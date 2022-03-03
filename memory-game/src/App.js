0dbc5d5b5303b19596a5f6485d34181fcc6c275c
alex_maco34 <alex_maco34@libero.it>
2022-03-03 14:56:33 +0100

    Third Two Hour Project, added randomizer for colors, now returning object with randomized color and number for id selection

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -27,9 +27,45 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
   const classes = useStyles();
+  
+  let ammountOfSquares = 16;
+  let ammountOfColors = ammountOfSquares/2;
+  let ammountOfMoves = ammountOfSquares/2;
+  let revealed = false;
+  let squareSize = "21%";
+  let unrevealedSquareColor = "gray";
+  let revealedColor = unrevealedSquareColor;
+  let squaresArray = Array(ammountOfSquares);
+  let coloredSquares = {};
+
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+  }
+
+  function enumerateArray(array){
+    for(let i=0; i<array.length; i++){
+      array[i] = i
+    }
+    return array
+  }
+
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = getRandomInt(currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
   }
 
   function getRandomColor() {
@@ -41,38 +77,26 @@ function App() {
     return color;
   }
 
-  function getTwoDifferentNumbers(max){
-    let firstNumber = getRandomInt(max);
-    let secondNumber = 0;
-    do {secondNumber = getRandomInt(max)
-    } while(secondNumber == firstNumber);
-    return [firstNumber, secondNumber]
-  }
+  squaresArray = enumerateArray(squaresArray)
+  squaresArray = shuffle(squaresArray)
 
-  let ammountOfSquares = 16;
-  let ammountOfColors = ammountOfSquares/2;
-  let ammountOfMoves = ammountOfSquares/2;
-  let revealed = false;
-  let squareSize = "21%";
-  let unrevealedSquareColor = "gray";
-
-  function colorSquares(numberOfSquares){
-    let squaresToReturn = {}
-    for(let i=0;i<numberOfSquares;i++){
+  function createColoredSquares(numberOfSquares, array){
+    let squaresToReturn = [{}];
+    for(let i=0;i<numberOfSquares-1;i++){
       let randomColor = getRandomColor()
-      twoNumbers
-      squaresToReturn.push({})
+      squaresToReturn.push({[randomColor]:array[i]});
     }
     return squaresToReturn
   }
 
-  let coloredSquares = colorSquares();
-  
+  coloredSquares = createColoredSquares(ammountOfSquares, squaresArray)
+  console.log(coloredSquares)
+
   function createCards(){
     return(
       <Grid container className={classes.squareContainer}>
         {Array.from(Array(ammountOfSquares)).map((_, index) => (
-            <Grid key={index} id={index} className={classes.singleSquare} style={{width: squareSize, height: squareSize, background: unrevealedSquareColor, }}>
+            <Grid key={index} id={index} className={classes.singleSquare} style={{width: squareSize, height: squareSize, background: revealedColor, }}>
             </Grid>
           ))}
       </Grid>
